00f4e478a149fb59639709d184f1f1028a0b64e4
zhangqiang <allenanswerzq@gmail.com>
2024-09-17 10:25:45 +0800

    use click as arg parse

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-import argparse
+import click
 
 from llmcc.parser import parse_from_file
 from llmcc.compiler import compile_graph
@@ -7,27 +7,21 @@ from llmcc.config import *
 from llmcc.includer import include_graph
 from llmcc.slicer import slice_graph
 
-
-def main():
-    parser = argparse.ArgumentParser(description="ai compiler")
-    parser.add_argument(
-        "-c", "--source", metavar="SOURCE", help="Source file to compile"
-    )
-    parser.add_argument("-I", "--include-dir", metavar="DIR", help="Include directory")
-    parser.add_argument("-o", "--output", metavar="OUTPUT", help="Output file")
-
-    args = parser.parse_args()
-
-    log.info(f"ai compiling {args.source}")
-    g = parse_from_file(args.source)
-    ng = include_graph(g, args.include_dir)
+@click.command()
+@click.option('-c', '--source', help='The source file to compile')
+@click.option('-I', '--include-dir',  help='Include directory')
+@click.option('-o', '--output', help="The output file")
+def main(source, include_dir, output):
+    log.info(f"ai compiling {source}")
+    g = parse_from_file(source)
+    ng = include_graph(g, include_dir)
     if ng:
         g = ng
     log.debug(g.root.text.decode("utf-8"))
     print_graph(g)
     slice_graph(g)
     compile_graph(g)
-    write_graph(g, args.output)
+    write_graph(g, output)
 
 
 if __name__ == "__main__":
